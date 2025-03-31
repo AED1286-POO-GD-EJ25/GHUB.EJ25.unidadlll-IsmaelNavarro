a209bf15041bebf64f0164b91102d41a71ce805c
Kevin Ismael Navarro Gomez <L24170650@culiacan.tecnm.mx>
2025-03-31 15:56:53 +0000

    lll

--- ventana/Ventana.java
+++ ventana/Ventana.java
@@ -1,11 +1,20 @@
 package ventana;
  public class Ventana {
+    private String marco1;
+    public String marco2;
+    protected String marco3;
+    String marco4;
  
-    public void copiar(Ventana w){
+    public  void copiar(Ventana w){
         System.out.println("Estoy en el metodo copiar de la clase base de Ventana");
 
     }
     public void copiar(String p, int x, int y){
-        System.err.println("Estoy en el metodo copiar de Ventana con String, int, int");
+        System.out.println("Estoy en el metodo copiar de Ventana con String, int, int");
+    }
+    public void metodo1(){
+        System.out.println(super.marco2);
+        System.out.println(super.marco3);
+        System.out.println(super.marco4);
     }
  }
